611df9ddf98b94069293cf959e65987907fb7140
Firestar99 <firestar99@sydow.cloud>
2025-04-21 13:32:01 +0200

    update to vulkan-sdk-1.4.309 (#5)
    * gitignore .idea
    
    * project: make spirv-tools-sys a member of the root workspace
    
    * ci: fix newer clippy lints
    
    * ci: update CI runner targets
    
    * ci: use old embark repo for comparing with __internal__binaries__ tag
    
    * update to vulkan-sdk-1.4.309
    
    * fix doctest
    
    * fix val test

--- tests/val.rs
+++ tests/val.rs
@@ -2,23 +2,23 @@
 
 const SPIRV_BIN: &[u8] = include_bytes!("wgpu_example_shader.spv");
 
-fn validate_compiled(_input: &[u8]) -> Option<Result<(), spirv_tools::Error>> {
+fn validate_compiled(_input: &[u32]) -> Option<Result<(), spirv_tools::Error>> {
     #[cfg(feature = "use-compiled-tools")]
     {
         use spirv_tools::val::{compiled::CompiledValidator, Validator};
         let cv = CompiledValidator::default();
-        Some(cv.validate(spirv_tools::binary::to_binary(_input).unwrap(), None))
+        Some(cv.validate(_input, None))
     }
     #[cfg(not(feature = "use-compiled-tools"))]
     None
 }
 
-fn validate_tool(_input: &[u8]) -> Option<Result<(), spirv_tools::Error>> {
+fn validate_tool(_input: &[u32]) -> Option<Result<(), spirv_tools::Error>> {
     #[cfg(feature = "use-installed-tools")]
     {
         use spirv_tools::val::{tool::ToolValidator, Validator};
         let cv = ToolValidator::default();
-        Some(cv.validate(spirv_tools::binary::to_binary(_input).unwrap(), None))
+        Some(cv.validate(_input, None))
     }
     #[cfg(not(feature = "use-installed-tools"))]
     None
@@ -29,9 +29,14 @@ fn gets_error_message() {
     let expected_msg = "error:0:0 - Loop header '6[%loop_header]' is targeted by 2 back-edge blocks but the standard requires exactly one";
     let expected_notes = "  %loop_header = OpLabel\n";
 
-    for res in validate_compiled(SPIRV_BIN)
+    let spirv = SPIRV_BIN
+        .chunks_exact(size_of::<u32>())
+        .map(|chunk| u32::from_ne_bytes(chunk.try_into().unwrap()))
+        .collect::<Vec<_>>();
+
+    for res in validate_compiled(&spirv)
         .into_iter()
-        .chain(validate_tool(SPIRV_BIN).into_iter())
+        .chain(validate_tool(&spirv).into_iter())
     {
         let err = res.unwrap_err();
         let diag = err.diagnostic.as_ref().unwrap();
